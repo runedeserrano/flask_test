6161727b1dee1f51ec9aa55dac2920cd4c82f71b
runedeserrano <runedeserrano@gmail.com>
2025-04-28 17:13:46 +0200

    changing between two pages with button

--- app.py
+++ app.py
@@ -4,5 +4,10 @@ app = Flask(__name__)
 @app.route('/home')
 def home():
    return render_template('home.html')
+
+@app.route('/other')
+def other():
+   return render_template('other.html')
+
 if __name__ == '__main__':
    app.run(debug=True)
